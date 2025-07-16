1310b52b967bf861f60572dfec89210170a05dc8
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2025-07-16 22:52:02 +0200

    update draft usecase to populate next queue

--- src/usecases/start_payment.go
+++ src/usecases/start_payment.go
@@ -3,20 +3,25 @@ package usecases
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/jhonatanlteodoro/payment_system/src/ports"
 	"github.com/jhonatanlteodoro/payment_system/src/types"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"time"
 )
 
 type StartPaymentUseCase struct {
 	queue   ports.Queue
-	cacheDB any
+	cacheDB ports.DistributedLock
+
+	processPaymentQueue ports.Queue
 }
 
-func NewStartPaymentUseCase(queue ports.Queue) *StartPaymentUseCase {
+func NewStartPaymentUseCase(queue, processPaymentQueue ports.Queue) *StartPaymentUseCase {
 	return &StartPaymentUseCase{
-		queue: queue,
+		queue:               queue,
+		processPaymentQueue: processPaymentQueue,
 	}
 }
 
@@ -35,7 +40,32 @@ func (m *StartPaymentUseCase) StartPayment(ctx context.Context, data *types.Paym
 }
 
 func (m *StartPaymentUseCase) workerStartPayment(delivery amqp.Delivery) error {
+	data := &types.Payment{}
+	if err := json.Unmarshal(delivery.Body, data); err != nil {
+		return err
+	}
 
+	if !m.cacheDB.AcquireLock(context.TODO(), data.FromAccount) {
+		log.Println("Account is locked. Enqueuing msg")
+		m.StartPayment(context.Background(), data)
+		return nil
+	}
+
+	fmt.Println("Lock Acquired")
+	fmt.Println("Processing something...")
+	fmt.Println("Updating the database...")
+	fmt.Println("generating next msg...")
+	time.Sleep(1 * time.Second)
+
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	if err := m.processPaymentQueue.Publish(context.TODO(), payload); err != nil {
+		return err
+	}
+
+	log.Println("Message published for processing. into Payment Processing Queue")
 	return nil
 }
 
