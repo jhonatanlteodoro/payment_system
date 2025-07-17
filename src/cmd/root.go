18ce8a3fb2028312e99e32b3e6c4e9ce5843632c
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2025-07-17 11:15:19 +0200

    add full server cmd and update worker cmd

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -5,7 +5,9 @@ import (
 	"github.com/jhonatanlteodoro/payment_system/src/api"
 	"github.com/jhonatanlteodoro/payment_system/src/workers"
 	"github.com/spf13/cobra"
+	"log"
 	"os"
+	"sync"
 )
 
 var rootCmd = &cobra.Command{
@@ -25,10 +27,34 @@ func getCmdServer(shutdown chan os.Signal) *cobra.Command {
 	}
 }
 
+func getCmdFullServer(shutdown chan os.Signal) *cobra.Command {
+	return &cobra.Command{
+		Use:   "full-server",
+		Short: "Run full server - Api Server + All workers",
+		Run: func(cmd *cobra.Command, args []string) {
+			wg := &sync.WaitGroup{}
+			wg.Add(2)
+
+			log.Println("Stating full server...")
+			go func() {
+				defer wg.Done()
+				api.StartApiServer(shutdown)
+			}()
+
+			go func() {
+				defer wg.Done()
+				workers.RunAllWorkers(shutdown)
+			}()
+			wg.Wait()
+		},
+	}
+}
+
 func getCmdWorker(shutdown chan os.Signal) *cobra.Command {
 	var runWorker = &cobra.Command{
 		Use:     "worker",
 		Short:   "Run worker by name",
+		Long:    "Available workers: start-payment, process-payment, notify, ALL",
 		Example: "worker --name workerName",
 		Run: func(cmd *cobra.Command, args []string) {
 			workerName, _ := cmd.Flags().GetString("name")
@@ -38,9 +64,20 @@ func getCmdWorker(shutdown chan os.Signal) *cobra.Command {
 				os.Exit(1)
 			}
 
-			fmt.Println("Starting worker: ", workerName)
-
-			workers.StartWorker(shutdown)
+			switch workerName {
+			case "ALL":
+				workers.RunAllWorkers(shutdown)
+			case "start-payment":
+				workers.RunStartPaymentWorker(shutdown)
+			case "process-payment":
+				workers.RunProcessPaymentWorker(shutdown)
+			case "notify":
+				workers.RunNotifyWorker(shutdown)
+			default:
+				fmt.Println("Please provide a valid worker name")
+				cmd.Usage()
+				os.Exit(1)
+			}
 		},
 	}
 
@@ -53,6 +90,7 @@ func getCmdWorker(shutdown chan os.Signal) *cobra.Command {
 func Execute(shutdown chan os.Signal) {
 	rootCmd.AddCommand(getCmdServer(shutdown))
 	rootCmd.AddCommand(getCmdWorker(shutdown))
+	rootCmd.AddCommand(getCmdFullServer(shutdown))
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
