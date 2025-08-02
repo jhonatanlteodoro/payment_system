c1858589de3c50eaa0e77be0070cb1061b40bf23
Jhonatan Teodoro <mollitiamteodoro@gmail.com>
2025-08-02 15:16:35 +0200

    update types

--- frankstrain/stackoverflow_dashboard/py_graphql_api/src/types/posts.py
+++ frankstrain/stackoverflow_dashboard/py_graphql_api/src/types/posts.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class PostsQueryInterface(ABC):
@@ -9,4 +10,12 @@ class PostsQueryInterface(ABC):
 
     @abstractmethod
     def get_posts(self, page, page_limit):
+        pass
+
+    @abstractmethod
+    async def get_post_count_overtime(self, start_date: datetime, end_date: datetime):
+        pass
+
+    @abstractmethod
+    async def get_tags_count_withing_posts_overtime(self, start_date: datetime, end_date: datetime):
         pass
